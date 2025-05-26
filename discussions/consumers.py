8ec9d3819aa5726faa3d7265591c98f0c0bb641a
patrick mugambi <patrick@kali.patrick>
2025-05-26 18:59:02 +0300

    update comment section

--- discussions/consumers.py
+++ discussions/consumers.py
@@ -1,39 +1,27 @@
 
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+from .models import Comment, ContentAttachment # Make sure you import ContentAttachment
 
 class DiscussionConsumer(AsyncWebsocketConsumer):
-    """
-    Handles WebSocket connections for real-time discussion updates.
-    Each discussion has its own "group" for broadcasting comments.
-    """
     async def connect(self):
-        self.discussion_id = self.scope['url_route']['kwargs']['discussion_id']
+        self.discussion_id = self.scope['url_route']['kwargs']['discussion_pk']
         self.discussion_group_name = f'discussion_{self.discussion_id}'
 
-        # Join discussion group
         await self.channel_layer.group_add(
             self.discussion_group_name,
             self.channel_name
         )
-
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave discussion group
         await self.channel_layer.group_discard(
             self.discussion_group_name,
             self.channel_name
         )
 
-    # Receive message from WebSocket (not used for sending comments from client, but can be)
-    async def receive(self, text_data):
-        # This consumer is primarily for broadcasting new comments from the server
-        # If you wanted to allow clients to send messages via WebSocket,
-        # you would process text_data here.
-        pass
-
-    # Receive message from channel layer (e.g., a new comment from a view)
+    # Receive message from room group
     async def new_comment_notification(self, event):
         message = event['message']
 
@@ -42,3 +30,28 @@ class DiscussionConsumer(AsyncWebsocketConsumer):
             'type': 'new_comment_notification',
             'message': message
         }))
+
+    # You might also have a receive method if comments are posted directly via WebSocket
+    # For now, we assume comments are posted via HTTP POST and then broadcasted.
+
+# In your views.py, when sending the message:
+# (Already updated in the previous `views.py` provided, just confirming the structure)
+# channel_layer = get_channel_layer()
+# async_to_sync(channel_layer.group_send)(
+#     f'discussion_{pk}',
+#     {
+#         'type': 'new_comment_notification',
+#         'message': {
+#             'id': new_comment.id,
+#             'author': new_comment.author.username,
+#             'author_id': new_comment.author.id, # <-- Ensure this is sent!
+#             'content': new_comment.content,
+#             'created_at': new_comment.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+#             'is_flagged': new_comment.is_flagged,
+#             'flag_reason': new_comment.flag_reason,
+#             'votes': new_comment.votes,
+#             'parent_id': new_comment.parent.id if new_comment.parent else None, # <-- Send None if no parent
+#             'attachments': attachments_data
+#         }
+#     }
+# )
--- discussions/urls.py
+++ discussions/urls.py
@@ -1,10 +1,12 @@
-from . import views
 from django.urls import path
-
+from . import views
 app_name = "discussions"
-
 urlpatterns = [
     path('', views.discussion_list, name='discussion_list'),
-    path('discussion/<int:pk>/', views.discussion_detail, name='discussion_detail'),
-    path('discussion/new/', views.create_discussion, name='create_discussion'),
+    path('new/', views.create_discussion, name='create_discussion'),
+    path('<int:pk>/', views.discussion_detail, name='discussion_detail'),
+    path('upvote_comment/<int:pk>/', views.upvote_comment, name='upvote_comment'),
+    path('downvote_comment/<int:pk>/', views.downvote_comment, name='downvote_comment'),
+    path('delete_comment/<int:pk>/', views.delete_comment, name='delete_comment'),
+    path('<int:discussion_pk>/reply/<int:parent_pk>/', views.reply_to_comment, name='reply_to_comment'),
 ]
